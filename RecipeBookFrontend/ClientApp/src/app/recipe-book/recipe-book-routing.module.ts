723b7be0ccca2e70bebb0544bf81393f4991f165
User <User@ZALMAN>
2022-08-06 15:58:31 +0300

    Добавлена страница с рецептами

--- RecipeBookFrontend/ClientApp/src/app/recipe-book/recipe-book-routing.module.ts
+++ RecipeBookFrontend/ClientApp/src/app/recipe-book/recipe-book-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { RouterModule, Routes } from '@angular/router';
+import { RecipePageComponent } from './recipe-page/recipe-page.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
+const routes: Routes = [
+  {
+    path: "recipe-book",
+    component:MainPageComponent
+  },
+  {
+    path: "recipe",
+    component: RecipePageComponent
+  },
+  {
+    path: "**",
+    component:MainPageComponent
+  }
+
+]
 
 
 @NgModule({
@@ -9,7 +27,10 @@ import { CommonModule } from '@angular/common';
     
   ],
   imports: [
-    CommonModule
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class RecipeBookRoutingModule { }
