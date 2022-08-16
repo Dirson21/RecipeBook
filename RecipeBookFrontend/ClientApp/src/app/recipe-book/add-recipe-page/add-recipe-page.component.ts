b16ffa71958947cff93c8aeaf63db5a0005ef435
User <User@ZALMAN>
2022-08-16 17:38:37 +0300

    Отображение добавленной картинки

--- RecipeBookFrontend/ClientApp/src/app/recipe-book/add-recipe-page/add-recipe-page.component.ts
+++ RecipeBookFrontend/ClientApp/src/app/recipe-book/add-recipe-page/add-recipe-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -13,12 +13,39 @@ export class AddRecipePageComponent implements OnInit {
    10, 20, 30, 40, 50, 60, 70
   ];
 
+  public imgUrl!: string;
   constructor() { }
 
   formGroup!: FormGroup
   imageSrc!: string;
 
   ngOnInit(): void {
+ 
+   
+  }
+
+
+
+
+
+  onFileChanged(event:any ) {
+    console.log(event);
+
+    let files: FileList = event.target.files; 
+    console.log(files)
+    if (files.length == 0) return;
+
+    let recipeImage: HTMLImageElement  = document.getElementById("recipeImg") as  HTMLImageElement;
+    let reader: FileReader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      recipeImage.src = reader.result as string;
+      recipeImage.classList.add("image-preview");
+    
+    }
+
+     
+    
   }
 
  
